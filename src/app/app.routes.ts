43d7eb171c5e86d81c4e3520f26c8d7f1b59980e
Juan Avila <Juan_Avila3579360@jabil.com>
2025-01-28 14:44:52 -0600

    Initi A19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MainComponent } from './layout/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: MainComponent,
+    children: [
+      {
+        path: 'home', loadChildren: () => import('./pages/home.module').then(d => d.HomeModule)
+      }
+    ]
+  }
+];

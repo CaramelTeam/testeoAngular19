ed78c516a19e8c940e77ae60705f021692846486
Juan Avila <Juan_Avila3579360@jabil.com>
2025-02-06 14:15:39 -0600

    Teste prueba

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  standalone: true,
+  imports: [RouterOutlet]
 })
 export class HomeComponent {
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  imports: [
+    ButtonModule,
+    ColorPickerModule,
+    FormsModule
+  ],
 })
 export class LoginComponent {
 
+
+  color: string | null = null;
+
+  cambiarTem(){
+    const nuevoFondo = document.documentElement.style.getPropertyValue('--color-fondo') === 'white' ? 'black' : 'white';
+    const nuevoTexto = nuevoFondo === 'white' ? 'black' : 'white';
+
+    document.documentElement.style.setProperty('--color-fondo', this.color);
+    document.documentElement.style.setProperty('--color-texto', nuevoTexto);
+  }
 }
